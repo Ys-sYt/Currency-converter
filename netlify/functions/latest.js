bda31dd724b3dd1c5bc203318a0efc8900992338
Yoshito SHIMIZU <yoshiyoshi20239@gmail.com>
2025-08-12 02:14:20 +0900

    add function

---
+++ netlify/functions/latest.js
@@ -0,0 +1,13 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function(event, context) {
+  const appId = process.env.API_KEY; // Netlifyの環境変数
+  const url = `https://openexchangerates.org/api/latest.json?app_id=${appId}`;
+  const response = await fetch(url);
+  const data = await response.text();
+  return {
+    statusCode: response.status,
+    body: data,
+    headers: { 'Content-Type': 'application/json' }
+  };
+};
